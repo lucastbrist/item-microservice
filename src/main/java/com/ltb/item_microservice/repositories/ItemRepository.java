f3ebd42cfe28272d2165cd3ae400290e5fe064f9
Lucas Brist <lucastbrist@gmail.com>
2025-09-16 15:13:02 -0500

    made stuff work

--- src/main/java/com/ltb/item_microservice/repositories/ItemRepository.java
+++ src/main/java/com/ltb/item_microservice/repositories/ItemRepository.java
@@ -3,9 +3,7 @@ package com.ltb.item_microservice.repositories;
 
 import com.ltb.item_microservice.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Item findByName(String name);
 }
